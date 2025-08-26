0eb7506c9a2d64e141feea6891d8ee0fb889882c
Oliver <oliver.laura@ucb.edu.bo>
2025-08-26 22:42:08 +0900

    test dividir

--- src/sumador.js
+++ src/sumador.js
@@ -3,3 +3,13 @@ function sumar(a, b) {
 }
 
 export default sumar;
+
+function multiplicar(a,b){
+    return a*b;
+}
+
+
+function dividir(a,b){
+    return a/b;
+}
+export {multiplicar, sumar, dividir};
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,9 +1,19 @@
-import sumar from "./sumador.js";
+
+import {sumar, multiplicar, dividir} from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
 });
-
+describe("Multiplicar", () => {
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(3, 2)).toEqual(6);
+  });
+})
+describe("Dividir", () => {
+  it("deberia dividir dos numeros", () => {
+    expect(dividir(6, 2)).toEqual(3);
+  });
+})
 
