51fc88f93631d084c8cfda8d5b7a32ea2e4658cc
Oliver <oliver.laura@ucb.edu.bo>
2025-09-02 08:30:41 +0900

    marcador judador 1  anota 3  punto 40-30 test

--- src/tenisPrueba.test.js
+++ src/tenisPrueba.test.js
@@ -35,3 +35,12 @@ test('jugador 2 anota 2 puntos: 30-30', () => {
     juego.anotarJugador2();
     expect(juego.obtenerMarcador()).toBe('30-30');
 } );
+test('jugador 1 anota 3 puntos: 40-30', () => {
+    const juego = new JuegoTenis();
+    juego.anotarJugador1();
+    juego.anotarJugador1();
+    juego.anotarJugador1();
+    juego.anotarJugador2();
+    juego.anotarJugador2();
+    expect(juego.obtenerMarcador()).toBe('40-30');
+} );  
