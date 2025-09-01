269c3244548fb738c500b35da77b8575da4756a3
Oliver <oliver.laura@ucb.edu.bo>
2025-09-02 08:08:10 +0900

    marcador inicial 15-0 test

--- src/tenisPrueba.test.js
+++ src/tenisPrueba.test.js
@@ -1,8 +1,15 @@
 
+
 import JuegoTenis, { Tenis } from "./tenis.js";
 
 test('Marcador inicial 0-0', () => {
     const juego = new JuegoTenis();
     expect(juego.obtenerMarcador()).toBe('Love-Love');
+} );
+
+test('Jugador 1 anota un punto: 15-0', () => {
+    const juego = new JuegoTenis();
+    juego.anotarJugador1();
+    expect(juego.obtenerMarcador()).toBe('15-Love');
 } );
  
